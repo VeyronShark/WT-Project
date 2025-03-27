4a120fee90a478b5d0647c26050eb1755176605f
VeyronShark <varun.satapathy@gmail.com>
2025-03-27 09:05:51 +0530

    Update connect.php
    added default connect values

--- connect.php
+++ connect.php
@@ -1,3 +1,4 @@
 <?php
-	$dbc = mysqli_connect('sql206.infinityfree.com', 'if0_38600511', 'P52f4MDQ88', 'if0_38600511_productivity');
+	//$dbc = mysqli_connect('sql206.infinityfree.com', 'if0_38600511', 'P52f4MDQ88', 'if0_38600511_productivity');
+	$dbc = mysqli_connect('localhost', 'root', '', 'productivity');
 ?>
